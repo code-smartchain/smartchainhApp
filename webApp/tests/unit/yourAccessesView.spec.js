77caeaad6309e709807d11ddabba50049dffdfa4
8BitJonny <timon99@gmx.de>
2019-04-13 15:59:40 +0200

    :rotating_light: Fixed UI Tests

--- webApp/tests/unit/yourAccessesView.spec.js
+++ webApp/tests/unit/yourAccessesView.spec.js
@@ -1,26 +1,43 @@
 import { expect } from 'chai';
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import YourAccesses from '@/views/YourAccesses.vue';
 import { connect } from "@holochain/hc-web-client";
+import Vuetify from 'vuetify/lib';
+import Vue from "vue";
 
 const $holochain = connect("ws:localhost:8887");
 
 describe('YourAccesses.vue', () => {
+  let wrapper = {};
+
+  beforeEach(() => {
+    Vue.use(Vuetify);
+
+    wrapper = shallowMount(YourAccesses, {
+      Vue,
+      mocks: { $holochain },
+      attachToDocument: true
+    });
+  });
+
   it('has a mounted hook', () => {
     expect(YourAccesses.mounted).to.be.a('function')
   });
   
   it('displays the shared Accesses', () => {
-    const wrapper = mount(YourAccesses, { mocks: { $holochain }, attachToDocument: true });
-
     const access = {
-      device_id: 'test device',
-      device_type: 'test device type',
-      device_name: 'test device name',
-      public_key: 'test public key',
-      description: 'test description',
-      transaction_hash: '',
-      time_restriction: '',
+      access: {
+        device_id: 'test device',
+        device_type: 'test device type',
+        device_name: 'test device name',
+        public_key: 'test public key',
+        description: 'test description',
+        transaction_hash: '',
+        time_restriction: '',
+      },
+      lock: {
+        device_id: 'test device'
+      }
     }
 
     wrapper.setData({ accesses: [access] })
@@ -29,8 +46,6 @@ describe('YourAccesses.vue', () => {
   });
 
   it('has a footer', () => {
-    const wrapper = mount(YourAccesses, { mocks: { $holochain }, attachToDocument: true });
-
     expect(wrapper.find('.footer').exists()).to.equal(true)
   });
 
