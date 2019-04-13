10e3f2b2781d4c93321793de833b4a2bc4d6b598
8BitJonny <timon99@gmx.de>
2019-04-13 19:05:56 +0200

    :rotating_light: enabled Vuetify for the OpenLock View

--- webApp/tests/unit/openLockView.spec.js
+++ webApp/tests/unit/openLockView.spec.js
@@ -1,27 +1,34 @@
 import { expect } from 'chai';
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import OpenLock from '@/views/OpenLock.vue';
+import Vuetify from 'vuetify/lib';
+import Vue from "vue";
 
 describe('OpenLock.vue', () => {
+  let wrapper = {};
+
+  beforeEach(() => {
+    Vue.use(Vuetify);
+
+    wrapper = shallowMount(OpenLock, {
+      Vue,
+      attachToDocument: true
+    });
+  });
+  
   it('has a mounted hook', () => {
     expect(OpenLock.mounted).to.be.a('function')
   });
 
-  it('has a info how to hold the phone', () => {
-    const wrapper = mount(OpenLock, { attachToDocument: true });
-    
+  it('has a info how to hold the phone', () => { 
     expect(wrapper.find('.info_hold').text()).to.equal("Hold your phone next to a lock")
   });
 
   it('has a footer', () => {
-    const wrapper = mount(OpenLock, { attachToDocument: true });
-
     expect(wrapper.find('.footer').exists()).to.equal(true)
   });
 
   it('has 3 circles for animation', () => {
-    const wrapper = mount(OpenLock, { attachToDocument: true });
-
     expect(wrapper.findAll('.circle')).to.have.lengthOf(3);
   });
 
