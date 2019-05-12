32dad05d3903a13061da0073f8909b1c2fd482ce
8BitJonny <timon99@gmx.de>
2019-05-12 16:27:20 +0200

    :rocket: Added Google Kubernetes Deployment

---
+++ deploy-prod.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+docker build -t eu.gcr.io/${PROJECT_ID}/${IMAGE_NAME}:$TRAVIS_COMMIT .
+
+echo $GOOGLE_APPLICATION_CREDENTIALS | base64 --decode -i > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+gcloud --quiet config set project $PROJECT_ID
+gcloud --quiet config set container/cluster $CLUSTER
+gcloud --quiet config set compute/zone $ZONE
+gcloud --quiet container clusters get-credentials $CLUSTER
+
+gcloud docker push eu.gcr.io/${PROJECT_ID}/${IMAGE_NAME}
+
+yes | gcloud beta container images add-tag eu.gcr.io/${PROJECT_ID}/${IMAGE_NAME}:$TRAVIS_COMMIT eu.gcr.io/${PROJECT_ID}/${IMAGE_NAME}:latest
+
+kubectl config view
+kubectl config current-context
+
+kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=eu.gcr.io/${PROJECT_ID}/${IMAGE_NAME}:$TRAVIS_COMMIT
+
+# sleep 30
